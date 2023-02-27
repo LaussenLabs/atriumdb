d43c9be16c08aae73abd4ce47a8826df3809c760
WilliamDixon <52508794+WilliamDixon@users.noreply.github.com>
2023-02-27 13:18:41 -0500

    Add docs for overwrite and protected_mode.

--- sdk/atriumdb/atrium_sdk.py
+++ sdk/atriumdb/atrium_sdk.py
@@ -169,7 +169,8 @@ class AtriumSDK:
         A class method to create a new dataset.
 
         >>> from atriumdb import AtriumSDK
-        >>> sdk = AtriumSDK.create_dataset(dataset_location="./new_dataset", database_type="sqlite")
+        >>> protected_mode, overwrite = None, None  # Use default values from `sdk/atriumdb/helpers/config.toml`
+        >>> sdk = AtriumSDK.create_dataset(dataset_location="./new_dataset", database_type="sqlite", protected_mode=protected_mode, overwrite=overwrite)
 
         >>> # MySQL/MariaDB Connection
         >>> connection_params = {
@@ -179,17 +180,18 @@ class AtriumSDK:
         >>>     'database': "new_dataset",
         >>>     'port': 3306
         >>> }
-        >>> sdk = AtriumSDK.create_dataset(dataset_location="./new_dataset", database_type="mysql", connection_params=connection_params)
+        >>> sdk = AtriumSDK.create_dataset(dataset_location="./new_dataset", database_type="mysql", protected_mode="False", overwrite="error", connection_params=connection_params)
 
         :param Union[str, PurePath] dataset_location: A file path or a path-like object that points to the directory in which the dataset will be written.
         :param str database_type: Specifies the type of metadata database to use. Options are "sqlite", "mysql", or "mariadb".
-        :param str protected_mode: Specifies the protection mode of the metadata database.
-        :param str overwrite: Specifies whether to overwrite an existing dataset at the specified location.
+        :param str protected_mode: Specifies the protection mode of the metadata database. Allowed values are "True" or "False". If "True", data deletion will not be allowed. If "False", data deletion will be allowed. The default behavior can be changed in the `sdk/atriumdb/helpers/config.toml` file.
+        :param str overwrite: Specifies the behavior to take when new data being inserted overlaps in time with existing data. Allowed values are "error", "ignore", or "overwrite". Upon triggered overwrite: if "error", an error will be raised. If "ignore", the new data will not be inserted. If "overwrite", the old data will be overwritten with the new data. The default behavior can be changed in the `sdk/atriumdb/helpers/config.toml` file.
         :param dict connection_params: A dictionary containing connection parameters for "mysql" or "mariadb" database type. It should contain keys for 'host', 'user', 'password', 'database', and 'port'.
 
         :return: An initialized AtriumSDK object.
         :rtype: AtriumSDK
         """
+
 
         # Create Dataset Directory if it doesn't exist.
         dataset_location = Path(dataset_location)
