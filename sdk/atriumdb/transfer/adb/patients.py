730a9681e89d06ec84f5f893567c1f70829f073e
WilliamDixon <52508794+WilliamDixon@users.noreply.github.com>
2023-12-13 11:58:02 -0500

    Add transfer_patient_table

--- sdk/atriumdb/transfer/adb/patients.py
+++ sdk/atriumdb/transfer/adb/patients.py
@@ -101,3 +101,56 @@ def validate_csv_dict(csv_dict: Dict, patient_id_list: list) -> bool:
 def generate_patient_ids(number_of_patients):
     # Generate the list of unique patient IDs
     return random.sample(range(10000, 10000 + 2 * number_of_patients), number_of_patients)
+
+
+def transfer_patient_table(from_sdk, to_sdk, patient_id_list, patient_id_map, patient_info_to_transfer=None):
+    """
+    Transfers patient information from one SDK to another.
+
+    :param from_sdk: The SDK instance to transfer data from.
+    :param to_sdk: The SDK instance to transfer data to.
+    :param patient_id_list: List of patient IDs to transfer.
+    :param patient_id_map: A mapping of old patient IDs to new patient IDs.
+    :param patient_info_to_transfer: List of patient info keys to transfer. If None, all info is transferred.
+    """
+
+    # Retrieve all patients from the from_sdk
+    all_patients = from_sdk.get_all_patients()
+
+    if len(all_patients) == 0:
+        return
+
+    validate_patient_info_to_transfer(patient_info_to_transfer, all_patients)
+
+    # Iterate over the patient IDs to be transferred
+    for patient_id in patient_id_list:
+        # Retrieve patient info based on patient_id
+        patient_info = all_patients.get(patient_id)
+
+        # If patient info exists, proceed with transfer
+        if patient_info:
+            # Use patient_id_map to get the new patient ID (if deidentified)
+            new_patient_id = patient_id_map.get(patient_id, patient_id)
+
+            # Filter patient info if patient_info_to_transfer is specified
+            if patient_info_to_transfer and patient_info_to_transfer != "all":
+                patient_info = {key: patient_info[key] for key in patient_info_to_transfer if key in patient_info}
+
+            # Insert patient info into to_sdk
+            to_sdk.insert_patient(patient_id=new_patient_id, **patient_info)
+        else:
+            print(f"Warning: Patient ID {patient_id} not found in from_sdk.")
+
+
+def validate_patient_info_to_transfer(patient_info_to_transfer, all_patients):
+    if patient_info_to_transfer and patient_info_to_transfer != "all":
+        if not isinstance(patient_info_to_transfer, list):
+            raise ValueError("patient_info_to_transfer, must be a list of patient "
+                             "info attributes or the string 'all' (or None)")
+
+        available_patient_info_keys = list(list(all_patients.values())[0].keys())
+        for key in patient_info_to_transfer:
+            if key not in available_patient_info_keys:
+                raise ValueError(
+                    f"key {key} from patient_info_to_transfer, not a valid key in patient_info. "
+                    f"Must be one of: {available_patient_info_keys}")
