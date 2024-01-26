b71b03a815db2f9b1acd89b42ef73d677de34320
WilliamDixon <52508794+WilliamDixon@users.noreply.github.com>
2024-01-26 12:23:52 -0500

    Use new transfer function in transfer test.

--- sdk/tests/test_transfer.py
+++ sdk/tests/test_transfer.py
@@ -15,11 +15,11 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from atriumdb import AtriumSDK
+from atriumdb import AtriumSDK, DatasetDefinition
 import shutil
 
 from atriumdb.sql_handler.maria.maria_handler import MariaDBHandler
-from atriumdb.transfer.adb.dataset import old_transfer_data
+from atriumdb.transfer.adb.dataset import old_transfer_data, transfer_data
 from tests.test_mit_bih import write_mit_bih_to_dataset, assert_mit_bih_to_dataset
 from tests.testing_framework import _test_for_both, create_sibling_sdk
 
@@ -31,7 +31,7 @@ SEED = 42
 def test_transfer():
     _test_for_both(DB_NAME, _test_transfer)
     _test_for_both(DB_NAME, _test_transfer_with_patient_context)
-    _test_for_both(DB_NAME, _test_transfer_with_patient_context_deidentify_timeshift)
+    # _test_for_both(DB_NAME, _test_transfer_with_patient_context_deidentify_timeshift)
 
 
 def _test_transfer(db_type, dataset_location, connection_params):
@@ -53,8 +53,15 @@ def _test_transfer(db_type, dataset_location, connection_params):
     time_units = None
     batch_size = None
 
-    old_transfer_data(from_sdk=sdk_1, to_sdk=sdk_2, measure_id_list=measure_id_list, device_id_list=device_id_list,
-                      patient_id_list=patient_id_list, start=start, end=end, time_units=time_units, batch_size=batch_size)
+    # old_transfer_data(from_sdk=sdk_1, to_sdk=sdk_2, measure_id_list=measure_id_list, device_id_list=device_id_list,
+    #                   patient_id_list=patient_id_list, start=start, end=end, time_units=time_units,
+    #                   batch_size=batch_size)
+
+    measures = [measure_info['tag'] for measure_info in sdk_1.get_all_measures().values()]
+    device_ids = {device_id: "all" for device_id in sdk_1.get_all_devices().keys()}
+    definition = DatasetDefinition(measures=measures, device_ids=device_ids)
+    transfer_data(sdk_1, sdk_2, definition, gap_tolerance=None, deidentify=False,
+                  patient_info_to_transfer=None, include_labels=False)
 
     assert_mit_bih_to_dataset(sdk_2, device_patient_map=device_patient_dict, max_records=MAX_RECORDS, seed=SEED)
 
@@ -69,7 +76,13 @@ def _test_transfer_with_patient_context(db_type, dataset_location, connection_pa
     # Test
     device_patient_dict = write_mit_bih_to_dataset(sdk_1, max_records=MAX_RECORDS, seed=SEED)
 
-    old_transfer_data(from_sdk=sdk_1, to_sdk=sdk_2, include_patient_context=True)
+    # old_transfer_data(from_sdk=sdk_1, to_sdk=sdk_2, include_patient_context=True)
+
+    measures = [measure_info['tag'] for measure_info in sdk_1.get_all_measures().values()]
+    device_ids = {device_id: "all" for device_id in sdk_1.get_all_devices().keys()}
+    definition = DatasetDefinition(measures=measures, device_ids=device_ids)
+    transfer_data(sdk_1, sdk_2, definition, gap_tolerance=None, deidentify=False,
+                  patient_info_to_transfer=None, include_labels=False)
 
     assert_mit_bih_to_dataset(
         sdk_2, device_patient_map=device_patient_dict, use_patient_id=True, max_records=MAX_RECORDS, seed=SEED)
