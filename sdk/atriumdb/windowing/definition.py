dd8e4bb060c8b7a548678c895739f01a8a016816
WilliamDixon <52508794+WilliamDixon@users.noreply.github.com>
2024-09-24 15:41:48 -0400

    Remove warning that judges the units of data based on the scale of the number, too many exceptions make it warn needlessly.

--- sdk/atriumdb/windowing/definition.py
+++ sdk/atriumdb/windowing/definition.py
@@ -265,10 +265,6 @@ class DatasetDefinition:
                 if key in ['pre', 'post'] and value < 0:
                     raise ValueError(f"{source_type} {source_id}: {key} cannot be negative")
 
-                if value < 1e9 or (value < 1e16 and key in ['start', 'end', 'time0']):
-                    warnings.warn(f"{source_type} {source_id}: The epoch for {key}: {value} looks like it's "
-                                  f"formatted in seconds. However {key} will be interpreted as nanosecond data.")
-
             if ('pre' in time_dict or 'post' in time_dict) and 'time0' not in time_dict:
                 raise ValueError(f"{source_type} {source_id}: 'pre' and 'post' cannot be provided without 'time0'")
 
