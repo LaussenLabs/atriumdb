860dc8645e4faf2d233ed6d1c6e669d973dcce2a
WilliamDixon <52508794+WilliamDixon@users.noreply.github.com>
2024-10-17 12:02:10 -0400

    interpret 0 scale_m as 1.

--- sdk/atriumdb/block.py
+++ sdk/atriumdb/block.py
@@ -250,7 +250,7 @@ class Block:
         logging.debug("Applying Scale Factors")
         logging.debug("------------------------")
         start_bench_scale = time.perf_counter()
-        scale_m_array = np.array([h.scale_m for h in headers])
+        scale_m_array = np.array([h.scale_m if h.scale_m != 0 else 1.0 for h in headers])
         if analog and not np.all(scale_m_array == 0):
             start_bench = time.perf_counter()
             scale_b_array = np.array([h.scale_b for h in headers])
@@ -358,7 +358,7 @@ class Block:
             time_data = merge_interval_data(time_data, period_ns)
 
         # Apply the scale factors to the value data if necessary
-        scale_m_array = np.array([h.scale_m for h in headers])
+        scale_m_array = np.array([h.scale_m if h.scale_m != 0 else 1.0 for h in headers])
         scale_b_array = np.array([h.scale_b for h in headers])
 
         if headers[0].v_raw_type == V_TYPE_INT64:
